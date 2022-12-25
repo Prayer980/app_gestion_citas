efd7310123eda020c4a2ee509bb441e9aac28b1d
Prayer980 <113539473+Prayer980@users.noreply.github.com>
2022-12-25 13:19:22 +0100

    Update CConstantes.cs

--- AppGestionCitas/CConstantes.cs
+++ AppGestionCitas/CConstantes.cs
@@ -8,9 +8,7 @@ namespace AppGestionCitas
 {
     class CConstantes
     {
-        //public const string connString = "Server=192.168.1.56\\PRODETIQV3;Database=GestionCitas;User Id=sa;Password=Di@gonal2111!!;";
-        public const string connString = @"Server=localhost\SQLEXPRESS;Database=GestionCitas;User Id=sa;Password=1234;";
-        //public const string connString = @"Server=localhost;Database=GestionCitas;User Id=sa;Password=1234;";
+        public const string connString = @"Server=localhost\SQLEXPRESS;Database=GestionCitas;User Id=;Password=;";
 
     }
 }
